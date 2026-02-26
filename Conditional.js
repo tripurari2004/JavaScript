7d3ec438960df0c60111e01811e24e19cb8177d2
tripurari2004 <tripurarikumar18feb@gmail.com>
2026-02-26 19:18:40 +0530

    Add conditional operators tutorial covering if/else chains

---
+++ Conditional.js
@@ -0,0 +1,31 @@
+let age = 18;
+
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
+
+//Boolean condition
+let isRaining = 0;
+
+if (isRaining) {
+  console.log("Don't forget to take an umbrella!");
+} else {
+  console.log("Enjoy the sunny weather!");
+}
+
+//Serveal conditions
+let score = 85;
+
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else if (score >= 70) {
+  console.log("Grade: C");
+} else if (score >= 60) {
+  console.log("Grade: D");
+} else {
+  console.log("Grade: F");
+}
